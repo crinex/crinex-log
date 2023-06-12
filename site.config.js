8b617c4f8b969c1cae0e89bf4ad3dd2f6aa1b7b2
Park June Young <crinexk@gmail.com>
2023-06-12 11:42:20 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "June-Young Park",
+    image: "/myavatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "AI Research",
+    bio: "I develop everything using python",
+    email: "wnsdud1007@naver.com",
+    linkedin: "crinex",
+    github: "crinex",
+    instagram: "liliilliill98",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `AUNAL`,
+      href: "https://aunal.org",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Crinex DevLog",
+    description: "welcome to Crinex's Dev Blog",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
@@ -51,7 +51,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
